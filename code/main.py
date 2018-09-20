c4cc8242d0cf9cb8d65b772dc8122d58f9981a14
yunatoomi <34559190+yunatoomi@users.noreply.github.com>
2018-09-20 08:16:17 +0300

    Update main.py
    Change to qt5

--- code/main.py
+++ code/main.py
@@ -1,38 +1,40 @@
 __author__ = 'Caroline Beyne'
 
-from PyQt4 import QtGui, QtCore
+__author__ = 'Caroline Beyne'
+
+from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
 from FrameLayout import FrameLayout
 
 if __name__ == '__main__':
 
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
 
-    win = QtGui.QMainWindow()
-    w = QtGui.QWidget()
+    win = QtWidgets.QMainWindow()
+    w = QtWidgets.QWidget()
     w.setMinimumWidth(350)
     win.setCentralWidget(w)
-    l = QtGui.QVBoxLayout()
+    l = QtWidgets.QVBoxLayout()
     l.setSpacing(0)
     l.setAlignment(QtCore.Qt.AlignTop)
     w.setLayout(l)
 
     t = FrameLayout(title="Buttons")
-    t.addWidget(QtGui.QPushButton('a'))
-    t.addWidget(QtGui.QPushButton('b'))
-    t.addWidget(QtGui.QPushButton('c'))
+    t.addWidget(QtWidgets.QPushButton('a'))
+    t.addWidget(QtWidgets.QPushButton('b'))
+    t.addWidget(QtWidgets.QPushButton('c'))
 
     f = FrameLayout(title="TableWidget")
     rows, cols = (6, 3)
     data = {'col1': ['1', '2', '3', '4', '5', '6'],
             'col2': ['7', '8', '9', '10', '11', '12'],
             'col3': ['13', '14', '15', '16', '17', '18']}
-    table = QtGui.QTableWidget(rows, cols)
+    table = QtWidgets.QTableWidget(rows, cols)
     headers = []
     for n, key in enumerate(sorted(data.keys())):
         headers.append(key)
         for m, item in enumerate(data[key]):
-            newitem = QtGui.QTableWidgetItem(item)
+            newitem = QtWidgets.QTableWidgetItem(item)
             table.setItem(m, n, newitem)
     table.setHorizontalHeaderLabels(headers)
     f.addWidget(table)
@@ -42,6 +44,5 @@ if __name__ == '__main__':
 
     win.show()
     win.raise_()
-    print "Finish"
+    print ("Finish")
     sys.exit(app.exec_())
-
